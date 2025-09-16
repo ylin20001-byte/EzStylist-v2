69d7bf6aa6f8cab12c66a98cc6ccc84e2fcf92c9
ylin20001-byte <ylin20001@gmail.com>
2025-09-16 17:46:53 -0400

    Delete ezstylist directory

--- ezstylist/components/AdjustmentPanel.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
-*/
-
-// This component is not used in the current application flow.
-const AdjustmentPanel = () => null;
-export default AdjustmentPanel;
--- ezstylist/wardrobe.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
-*/
-
-import { WardrobeItem } from './types';
-
-// Default wardrobe is empty, users will upload their own items.
-export const defaultWardrobe: WardrobeItem[] = [];
